819e3d41fa4562555972e7b04247803d75aa6df9
Murilo Martins <murilolmartins1@gmail.com>
2022-01-28 13:45:33 -0300

    feat: create  user_provider

--- src/Context/User/index.tsx
+++ src/Context/User/index.tsx
@@ -1,4 +1,62 @@
-export const a = 0;
-//         ^
-//         ^
-// pode apagar
+import jwtDecode from "jwt-decode";
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+import { userApi } from "Services/api";
+
+interface User {
+  email: string;
+  name: string;
+  age: number;
+  id: number;
+}
+
+interface TokenData {
+  email: string;
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+interface UserContextProps {
+  user: User;
+  getUser: () => void;
+}
+
+const UserContext = createContext<UserContextProps>({} as UserContextProps);
+
+export const UserProvider = ({ children }: UserProviderProps) => {
+  const [user, setUser] = useState<User>({} as User);
+
+  const getUser = useCallback(async () => {
+    const token = localStorage.getItem("@WatchList:Token") || "";
+    const tokenData: TokenData = jwtDecode(token);
+    const response = await userApi.get("/users");
+    const userData = await response.data.filter(
+      (user: User) => user.id === Number(tokenData.sub)
+    );
+    setUser(userData);
+  }, []);
+
+  //   const addSerie = async (serie) => {
+  //     const token = localStorage.getItem("@WatchList:Token") || "";
+  //     const response = await userApi.get("", {
+  //       headers: { Authorization: token },
+  //     });
+
+  return (
+    <UserContext.Provider value={{ getUser, user }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const UseUser = () => useContext(UserContext);
