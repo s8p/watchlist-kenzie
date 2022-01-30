9b85dda61f5f7c1b1f895eacf45bcfee506edba1
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2022-01-30 12:26:10 -0300

    style: implements dashboard background

--- src/Pages/Dashboard/style.ts
+++ src/Pages/Dashboard/style.ts
@@ -2,10 +2,27 @@ import styled from "@emotion/styled";
 import Background from "../../Assets/background.jpeg";
 
 export const Container = styled.div`
-  background: orange;
+  /* background: orange; */
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-image: url(${Background});
+  background-size: 80%;
+  .opacity_container {
+    position: absolute;
+    width: 100vw;
+    height: 100%;
+    /* background: green; */
+    /* opacity: 0.6; */
+    background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.586053) 0.01%,
+      rgba(0, 0, 0, 0) 0.02%,
+      rgba(0, 0, 0, 0.586053) 0.03%,
+      #000000 99.99%
+    );
+  }
 `;
 
 export const DashboardBox = styled.div`
@@ -13,7 +30,7 @@ export const DashboardBox = styled.div`
   color: white;
   text-align: center;
   /* height: 900px; */
-  background: var(--gray);
+  /* background: var(--gray); */
   margin-top: 30px;
 
   display: flex;
@@ -23,13 +40,14 @@ export const DashboardBox = styled.div`
   justify-content: space-evenly;
 
   > header {
-    background: red;
+    /* background: red; */
     width: 100%;
     height: 50px;
   }
 
   > section {
-    background: pink;
+    z-index: 0;
+    /* background: pink; */
     color: black;
     display: flex;
     flex-direction: column;
