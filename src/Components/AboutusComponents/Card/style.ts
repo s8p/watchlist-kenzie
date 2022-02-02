86dba9189a912e65c2ff224f0a5e606664cb5c91
Thiago Trad <thiago_trad@hotmail.com>
2022-02-02 10:17:38 -0300

    Feat: AboutUs Card style

--- src/Components/AboutusComponents/Card/style.ts
+++ src/Components/AboutusComponents/Card/style.ts
@@ -1,19 +1,36 @@
+import { keyframes } from "@emotion/react";
 import styled from "@emotion/styled";
+
+const rotateAnimation = keyframes`
+ 0% {
+  transform: rotateY(0deg);
+} 
+50% {
+  transform: rotateY(180deg);
+}
+100% {
+  transform: rotateY(360deg);
+}
+`;
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
-  background-color: purple;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 15px;
   flex: 1;
   max-width: 40%;
   min-width: 250px;
   max-width: 300px;
   height: 250px;
   margin-bottom: 20px;
+  color: var(--yellow);
   > header {
     padding: 20px;
-    width: 40%;
+    font-family: "Bungee", cursive;
+    display: flex;
+    justify-content: space-between;
   }
   > footer {
     padding: 20px;
@@ -21,16 +38,26 @@ export const Container = styled.div`
     display: flex;
     justify-content: space-around;
   }
+  :hover {
+    animation: ${rotateAnimation} 1s linear;
+  }
 `;
 
 export const CardInfo = styled.div`
   display: flex;
   flex: 1;
-  align-items: center;
+  align-items: start;
   padding: 20px;
+  padding-top: 0px;
   justify-content: space-between;
   > img {
-    max-width: 100px;
-    min-width: 80px;
+    width: 100px;
+    border-radius: 15px;
+  }
+  .list {
+    list-style-type: circle;
+    margin-left: 20px;
+    font-family: "Work Sans", sans-serif;
+    color: var(--white);
   }
 `;
